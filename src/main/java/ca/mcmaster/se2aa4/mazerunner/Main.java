7ff3233d09dedbc79b32fd05c44a6523d0c392c7
Emi Membere <tamimem005@gmail.com>
2025-01-23 00:08:44 -0500

    Replaced System.out.println with logger and implemented Apache CLI parsing

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -3,18 +3,41 @@ package ca.mcmaster.se2aa4.mazerunner;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import org.apache.commons.cli.*;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
+
+        Options options = new Options();
+        options.addOption("i", true, "Input maze file");
+
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = null;
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+            cmd = parser.parse(options, args);
+        } catch (ParseException e) {
+            logger.error("Error parsing command line: " + e.getMessage());
+            return;
+        }
+
+        if (!cmd.hasOption("i")) {
+            logger.error("Please provide the input file using -i option");
+            return;
+        }
+
+        String inputFile = cmd.getOptionValue("i");
+        logger.info("**** Reading the maze from file " + args[0]);
+            
+        try (BufferedReader reader = new BufferedReader(new FileReader(args[0]))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 for (int idx = 0; idx < line.length(); idx++) {
@@ -24,10 +47,10 @@ public class Main {
                         System.out.print("PASS ");
                     }
                 }
-                System.out.print(System.lineSeparator());
+                logger.info(System.lineSeparator());
             }
         } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+            logger.error("/!\\ An error has occured /!\\");
         }
         System.out.println("**** Computing path");
         System.out.println("PATH NOT COMPUTED");
