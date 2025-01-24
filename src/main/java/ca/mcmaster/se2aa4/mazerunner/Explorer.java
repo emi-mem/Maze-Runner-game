9fa003e6b14e6c7db43305523b5f4d9053339463
Emi Membere <tamimem005@gmail.com>
2025-01-23 23:34:18 -0500

    walking skeleton for explorer and path

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Explorer.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Explorer.java
@@ -1,5 +1,27 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
 public class Explorer {
-     
+    private int x, y;
+    private Maze maze;
+
+    public Explorer(Maze maze, int startX, int startY) {
+        this.maze = maze;
+        this.x = startX;
+        this.y = startY;
+    }
+
+    public void moveEast() {
+        if (!maze.isWall(x + 1, y)) {
+            x++;
+        }
+    }
+
+    public String getCurrentPosition() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public boolean canMove() {
+        // Dummy condition to stop moving, implement your logic
+        return x < 9;
+    }
 }
--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Path.java
@@ -1,5 +1,16 @@
 package ca.mcmaster.se2aa4.mazerunner;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Path {
-    
+    private List<String> steps = new ArrayList<>();
+
+    public void addStep(String step) {
+        steps.add(step);
+    }
+
+    public String getPath() {
+        return String.join(", ", steps);
+    }
 }
